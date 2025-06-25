f3207ee57b8e77671f766b4bdde824a5df2fb241
Damjan Kovačič <letecnez@gmail.com>
2025-06-26 01:40:54 +0200

    Version 0.2.0

--- poc/src/index.ts
+++ poc/src/index.ts
@@ -6,15 +6,13 @@ import multer from 'multer';
 import axios from 'axios';
 import FormData from 'form-data';
 
-const envFile = process.env.ENV_FILE || '.env';
+const envFile = process.env.ENV_FILE || '.env.prod';
 dotenv.config({
     path: path.resolve(process.cwd(), envFile),
 });
 
 const app = express();
 const PORT = process.env.PORT || 3666;
-const ONLINE_STATUS_CHECK_INTERVAL_IN_SECONDS = 60;
-const ONLINE_STATUS_CHECK_NOTIFY_AFTER_RETRIES = 5;
 
 // Set up multer for parsing multipart/form-data
 const upload = multer();
@@ -22,18 +20,39 @@ const upload = multer();
 // Middleware to parse incoming JSON requests
 app.use(bodyParser.json());
 
-const DISCORD_WEBHOOK = process.env.DISCORD_WEBHOOK;
-if (!DISCORD_WEBHOOK) {
+if (!process.env.VERSION)
+{
+    throw new Error('Missing VERSION environment variable!');
+}
+const VERSION = process.env.VERSION;
+
+if (!process.env.DISCORD_WEBHOOK) {
     throw new Error('Missing DISCORD_WEBHOOK environment variable!');
 }
+const DISCORD_WEBHOOK = process.env.DISCORD_WEBHOOK;
 
-const PLEX_URL_WITH_TOKEN = process.env.PLEX_URL_WITH_TOKEN;
-if (!PLEX_URL_WITH_TOKEN) {
+if (!process.env.PLEX_URL_WITH_TOKEN) {
     throw new Error('Missing PLEX_URL_WITH_TOKEN environment variable!');
 }
+const PLEX_URL_WITH_TOKEN = process.env.PLEX_URL_WITH_TOKEN;
+
+if (!process.env.ONLINE_STATUS_CHECK_INTERVAL_IN_SECONDS) {
+    throw new Error('Missing ONLINE_STATUS_CHECK_INTERVAL_IN_SECONDS environment variable!');
+}
+const ONLINE_STATUS_CHECK_INTERVAL_IN_SECONDS = Number.parseInt(process.env.ONLINE_STATUS_CHECK_INTERVAL_IN_SECONDS, 10);
+
+if (!process.env.ONLINE_STATUS_CHECK_NOTIFY_AFTER_RETRIES)
+{
+    throw new Error('Missing ONLINE_STATUS_CHECK_NOTIFY_AFTER_RETRIES environment variable!');
+}
+const ONLINE_STATUS_CHECK_NOTIFY_AFTER_RETRIES = Number.parseInt(process.env.ONLINE_STATUS_CHECK_NOTIFY_AFTER_RETRIES);
+
+let isOnline = true;
+let lastCheck = new Date(0);
+let lastOffline = new Date(0);
 
 app.get('', (req, res) => {
-    res.status(200).send(`plex-2-discord is running on port ${PORT}`);
+    res.status(200).send(`<strong>plex-2-discord service</strong><br />Author <strong>Damjan Kovačič</strong><br />Version <strong>${VERSION}</strong><br />Port <strong>${PORT}</strong><br />PLEX status <strong>${isOnline ? '✅ ONLINE' : '❌ OFFLINE'}</strong><br />Last check <strong>${lastCheck.toISOString()}</strong><br />Last offline <strong>${lastOffline.toISOString()}</strong>`);
 });
 
 app.post('/webhook', upload.any(), (req, res) => {
@@ -49,7 +68,7 @@ app.post('/webhook', upload.any(), (req, res) => {
     var libraryTitle = payload.Metadata.librarySectionTitle;
     var title = payload.Metadata.title;
     var summary = payload.Metadata.summary;
-    const maxSummaryLength = 200;
+    const maxSummaryLength = 400;
     if (summary.length > maxSummaryLength) {
         summary = summary.substring(0, maxSummaryLength - 3) + ' ...';
     }
@@ -61,7 +80,7 @@ app.post('/webhook', upload.any(), (req, res) => {
                 title: title,
                 description: summary,
                 color: 0xe5a00d,
-                thumbnail: {
+                image: {
                     url: 'attachment://thumb.jpg',
                 },
             },
@@ -88,30 +107,34 @@ app.post('/webhook', upload.any(), (req, res) => {
     res.status(200).send('Webhook received successfully');
 });
 
-let isOnline: boolean | null = null;
 let retries = 0;
-let notified = false;
+let notifiedOffline = false;
 
 const checkPlexStatus = () => {
+    lastCheck = new Date();
     axios
         .head(PLEX_URL_WITH_TOKEN, {
             timeout: 5000,
         })
         .then(() => {
-            if (!isOnline) {
+            isOnline = true;
+            if (notifiedOffline) {
                 console.log('notify online!');
                 notifyPlexStatus('✅ Plex server is now **online**!');
             }
-            isOnline = true;
-            notified = false;
+            notifiedOffline = false;
             retries = 0;
         })
         .catch((err) => {
+            if (isOnline)
+            {
+                lastOffline = new Date();
+            }
             isOnline = false;
             retries++;
-            if (!notified && retries >= ONLINE_STATUS_CHECK_NOTIFY_AFTER_RETRIES) {
+            if (!notifiedOffline && retries >= ONLINE_STATUS_CHECK_NOTIFY_AFTER_RETRIES) {
                 console.log('notify offline!');
-                notified = true;
+                notifiedOffline = true;
                 notifyPlexStatus('❌ Plex server is **offline**!');
             }
         });
@@ -121,7 +144,7 @@ const notifyPlexStatus = (content: string) => {
     axios.post(DISCORD_WEBHOOK, { content: content });
 };
 
-setInterval(checkPlexStatus, 60 * 1000);
+setInterval(checkPlexStatus, ONLINE_STATUS_CHECK_INTERVAL_IN_SECONDS * 1000);
 
 app.listen(PORT, () => {
     console.log(`plex-2-discord id running on port ${PORT}!`);
